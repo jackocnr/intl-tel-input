057cac42a9ed5d926cb965345b7bd0f2646720f7
Jack O'Connor <jackocnr@gmail.com>
2013-06-05 12:34:01 -0700

    converted from tabs to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,58 +1,58 @@
 module.exports = function(grunt) {
 
-	// config
-	grunt.initConfig({
-		sass: {
-			build: {
-				files: {
-					'build/css/intlTelInput.css': [
-						'src/css/intlTelInput.scss',
-						'src/css/flags16.scss'
-					],
-					'build/css/demo.css': 'src/css/demo.scss'
-				}
-			}
-		},
-		jshint: {
-			all: "src/js/**/*.js",
-			options: {
-				laxcomma: true
-			}
-		},
-		concat: {
-			dev: {
-				files: {
-				  'build/js/intlTelInput.js': ['src/js/**/*.js']
-				}
-			}
-		},
-		uglify: {
-			build: {
-	      files: {
-	        'build/js/intlTelInput.min.js': ['src/js/**/*.js']
-	      }
-			}
-	  },
-		watch: {
-			js: {
-				files: "src/js/**/*.js",
-				tasks: ["jshint", "concat"]
-			},
-			css: {
-				files: "src/css/**/*.scss",
-				tasks: "sass:build"
-			}
-		}
-	});
+  // config
+  grunt.initConfig({
+    sass: {
+      build: {
+        files: {
+          'build/css/intlTelInput.css': [
+            'src/css/intlTelInput.scss',
+            'src/css/flags16.scss'
+          ],
+          'build/css/demo.css': 'src/css/demo.scss'
+        }
+      }
+    },
+    jshint: {
+      all: "src/js/**/*.js",
+      options: {
+        laxcomma: true
+      }
+    },
+    concat: {
+      dev: {
+        files: {
+          'build/js/intlTelInput.js': ['src/js/**/*.js']
+        }
+      }
+    },
+    uglify: {
+      build: {
+        files: {
+          'build/js/intlTelInput.min.js': ['src/js/**/*.js']
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: "src/js/**/*.js",
+        tasks: ["jshint", "concat"]
+      },
+      css: {
+        files: "src/css/**/*.scss",
+        tasks: "sass:build"
+      }
+    }
+  });
 
-	// plugins
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-concat');
+  // plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
-	// tasks
-	grunt.registerTask('default', ['jshint', 'sass:build', 'uglify:build', 'concat:dev']);
+  // tasks
+  grunt.registerTask('default', ['jshint', 'sass:build', 'uglify:build', 'concat:dev']);
 
 };
--- src/js/intlTelInput.js
+++ src/js/intlTelInput.js
@@ -1,116 +1,116 @@
 ;(function ( $, window, document, undefined ) {
 
-    var pluginName = "intlTelInput",
-        defaults = {
-            // placeholder: "e.g. +44 1234567890",
-            // mobileVal: "+1 "
-        };
+  var pluginName = "intlTelInput",
+    defaults = {
+      // placeholder: "e.g. +44 1234567890",
+      // mobileVal: "+1 "
+    };
 
-    function Plugin( element, options ) {
-        this.element = element;
+  function Plugin( element, options ) {
+    this.element = element;
 
-        this.options = $.extend( {}, defaults, options );
+    this.options = $.extend( {}, defaults, options );
 
-        this._defaults = defaults;
-        this._name = pluginName;
+    this._defaults = defaults;
+    this._name = pluginName;
 
-        this.init();
-    }
+    this.init();
+  }
 
-    Plugin.prototype = {
+  Plugin.prototype = {
 
-        init: function() {
-          // telephone input
-          var telInput = $(this.element);
+    init: function() {
+      // telephone input
+      var telInput = $(this.element);
 
-          // containers
-          telInput.wrap($("<div>", {"class": "intl-number-input"}));
-          var flagsContainer = $("<div>", {"class": "flag-dropdown f16"}).insertBefore(telInput);
-          
-          // currently selected flag
-          var selectedFlag = $("<div>", {"class": "selected-flag"}).appendTo(flagsContainer);
-          $("<div>", {"class": "flag us"}).appendTo(selectedFlag);
-          
-          // country list contains: preferred countries, then divider, then all countries
-          var countryList = $("<ul>", {"class": "country-list hide"}).appendTo(flagsContainer);
-          this.intlNumberInputAppendListItems(preferredCountries, countryList);
-          $("<li>", {"class": "divider"}).appendTo(countryList);
-          this.intlNumberInputAppendListItems(countries, countryList);
+      // containers
+      telInput.wrap($("<div>", {"class": "intl-number-input"}));
+      var flagsContainer = $("<div>", {"class": "flag-dropdown f16"}).insertBefore(telInput);
+      
+      // currently selected flag
+      var selectedFlag = $("<div>", {"class": "selected-flag"}).appendTo(flagsContainer);
+      $("<div>", {"class": "flag us"}).appendTo(selectedFlag);
+      
+      // country list contains: preferred countries, then divider, then all countries
+      var countryList = $("<ul>", {"class": "country-list hide"}).appendTo(flagsContainer);
+      this.intlNumberInputAppendListItems(preferredCountries, countryList);
+      $("<li>", {"class": "divider"}).appendTo(countryList);
+      this.intlNumberInputAppendListItems(countries, countryList);
 
-          // update flag on keyup
-          telInput.keyup(function() {
-            var inputVal = telInput.val().trim();
-            // only interested in international numbers
-            if (inputVal.substring(0, 1) == "+") {
-              // strip out non-numeric chars
-              var num = inputVal.replace(/\D/g,'');
-              if (!isNaN(parseInt(num, 10))) {
-                // make it a string again, to run substring
-                var dialCode = ("" + parseInt(num, 10)).substring(0, 3);
-                // try first 3 digits, then 2 then 1
-                for (var i = dialCode.length; i > 0; i--) {
-                  dialCode = dialCode.substring(0, i);
-                  if (countryCodes[dialCode]) {
-                    // when we get a match, update the selected-flag
-                    var countryCode = countryCodes[dialCode][0].toLowerCase();
-                    selectedFlag.find(".flag").attr("class", "flag "+countryCode);
-                    break;
-                  }
-                }
+      // update flag on keyup
+      telInput.keyup(function() {
+        var inputVal = telInput.val().trim();
+        // only interested in international numbers
+        if (inputVal.substring(0, 1) == "+") {
+          // strip out non-numeric chars
+          var num = inputVal.replace(/\D/g,'');
+          if (!isNaN(parseInt(num, 10))) {
+            // make it a string again, to run substring
+            var dialCode = ("" + parseInt(num, 10)).substring(0, 3);
+            // try first 3 digits, then 2 then 1
+            for (var i = dialCode.length; i > 0; i--) {
+              dialCode = dialCode.substring(0, i);
+              if (countryCodes[dialCode]) {
+                // when we get a match, update the selected-flag
+                var countryCode = countryCodes[dialCode][0].toLowerCase();
+                selectedFlag.find(".flag").attr("class", "flag "+countryCode);
+                break;
               }
             }
-          });
-          // trigger it now
-          telInput.keyup();
-
-          // show country dropdown on click
-          selectedFlag.click(function(e) {
-            // prevent the click-off-to-close listener from firing
-            e.stopPropagation();
-            countryList.removeClass("hide");
-          });
-
-          // listen for country selection
-          countryList.find(".country").click(function(e) {
-            var countryCode = $(e.currentTarget).attr("data-country-code").toLowerCase();
-            selectedFlag.find(".flag").attr("class", "flag "+countryCode);
-            telInput.val("+" + $(e.currentTarget).attr("data-dial-code") + " ");
-            countryList.addClass("hide");
-          });
-
-          // click off to close
-          $('html').click(function(e) {
-            if (!$(e.target).closest('.country-list').length) {
-              countryList.addClass("hide");
-            }
-          });
-        },
-
-        // add a country <li> to the given <ul>
-        intlNumberInputAppendListItems: function(countryList, container) {
-          $.each(countryList, function(i, c) {
-            var listItem = $("<li>", {
-              "class": "country",
-              "data-dial-code": c['calling-code'],
-              "data-country-code": c.cca2
-            }).appendTo(container);
-
-            $("<div>", {
-              "class": "flag "+c.cca2.toLowerCase()
-            }).appendTo(listItem);
-
-            $("<span>", {"class": "country-name"}).text(c.name).appendTo(listItem);
-            $("<span>", {"class": "grey"}).text("+"+c['calling-code']).appendTo(listItem);
-          });
+          }
         }
-    };
+      });
+      // trigger it now
+      telInput.keyup();
 
-    $.fn[pluginName] = function ( options ) {
-        return this.each(function () {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
-            }
-        });
-    };
+      // show country dropdown on click
+      selectedFlag.click(function(e) {
+        // prevent the click-off-to-close listener from firing
+        e.stopPropagation();
+        countryList.removeClass("hide");
+      });
+
+      // listen for country selection
+      countryList.find(".country").click(function(e) {
+        var countryCode = $(e.currentTarget).attr("data-country-code").toLowerCase();
+        selectedFlag.find(".flag").attr("class", "flag "+countryCode);
+        telInput.val("+" + $(e.currentTarget).attr("data-dial-code") + " ");
+        countryList.addClass("hide");
+      });
+
+      // click off to close
+      $('html').click(function(e) {
+        if (!$(e.target).closest('.country-list').length) {
+          countryList.addClass("hide");
+        }
+      });
+    },
+
+    // add a country <li> to the given <ul>
+    intlNumberInputAppendListItems: function(countryList, container) {
+      $.each(countryList, function(i, c) {
+        var listItem = $("<li>", {
+          "class": "country",
+          "data-dial-code": c['calling-code'],
+          "data-country-code": c.cca2
+        }).appendTo(container);
+
+        $("<div>", {
+          "class": "flag "+c.cca2.toLowerCase()
+        }).appendTo(listItem);
+
+        $("<span>", {"class": "country-name"}).text(c.name).appendTo(listItem);
+        $("<span>", {"class": "grey"}).text("+"+c['calling-code']).appendTo(listItem);
+      });
+    }
+  };
+
+  $.fn[pluginName] = function ( options ) {
+      return this.each(function () {
+          if (!$.data(this, "plugin_" + pluginName)) {
+              $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
+          }
+      });
+  };
 
 })( jQuery, window, document );
