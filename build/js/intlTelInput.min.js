b2147e88aefb0d2a95d8cc892029c9915bedbd21
Jack O'Connor <jackocnr@gmail.com>
2014-01-31 19:50:59 -0800

    updated version to 0.9.14

--- build/js/intlTelInput.min.js
+++ build/js/intlTelInput.min.js
@@ -1,6 +1,6 @@
 /*
 intl-tel-input 
-version: 0.9.13
+version: 0.9.14
 description: A jQuery plugin for entering international telephone numbers
 repository: https://github.com/Bluefieldscom/intl-tel-input.git
 license: MIT
