80077e84082beed21914f390726811f47cf16109
Jack O'Connor <jackocnr@gmail.com>
2014-05-07 10:05:26 -0700

    Release v1.1.9

--- build/js/intlTelInput.js
+++ build/js/intlTelInput.js
@@ -1,5 +1,5 @@
 /*
-International Telephone Input v1.1.8
+International Telephone Input v1.1.9
 https://github.com/Bluefieldscom/intl-tel-input.git
 */
 // wrap in UMD - see https://github.com/umdjs/umd/blob/master/jqueryPlugin.js
