58239fd751088ed74ed1415907ab3108942ad039
Jack O'Connor <jackocnr@gmail.com>
2014-07-12 14:33:43 -0700

    tests for getSelectedCountryData

---
+++ src/spec/tests/methods/getSelectedCountryData.js
@@ -0,0 +1,29 @@
+"use strict";
+
+describe("init plugin to test public method getSelectedCountryData", function() {
+
+  beforeEach(function() {
+    input = $("<input>");
+    input.intlTelInput();
+  });
+
+  afterEach(function() {
+    input.intlTelInput("destroy");
+    input = null;
+  });
+  
+  it("gets the right default country data", function() {
+    expect(input.intlTelInput("getSelectedCountryData").iso2).toEqual("us");
+  });
+
+  it("change country by number gets the right country data", function() {
+    input.val("+44").keypress();
+    expect(input.intlTelInput("getSelectedCountryData").iso2).toEqual("gb");
+  });
+
+  it("change country by selecting a flag gets the right country data", function() {
+    selectFlag("ch");
+    expect(input.intlTelInput("getSelectedCountryData").iso2).toEqual("ch");
+  });
+
+});
