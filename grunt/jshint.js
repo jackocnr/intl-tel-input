ebcf0ffd9b0cc25c4986a1217c03959a97996722
Jack OConnor <jackocnr@gmail.com>
2015-01-10 19:00:40 -0800

    update libphonenumber to v7.0.2

--- grunt/jshint.js
+++ grunt/jshint.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
   return {
     dev: "src/js/**/*.js",
-    build: "tmp/all.js",
+    build: "tmp/wrapped.js",
     options: {
       // this is for data.js which sometimes has commas on the following line
       laxcomma: true
