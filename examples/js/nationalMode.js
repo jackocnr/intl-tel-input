c91dea3d5aeb4e7412bb6d2c3d3a35a612bd8395
Jack OConnor <jackocnr@gmail.com>
2015-02-06 21:00:19 -0800

    fix nationalMode example

--- examples/js/nationalMode.js
+++ examples/js/nationalMode.js
@@ -6,7 +6,8 @@ input.intlTelInput({
   utilsScript: "../../lib/libphonenumber/build/utils.js" // just for formatting/placeholders etc
 });
 
-input.keyup(function() {
+// listen to "keyup", but also "change" to update when the user selects a country
+input.on("keyup change", function() {
   var intlNumber = input.intlTelInput("getNumber");
   if (intlNumber) {
     output.text("International: " + intlNumber);
