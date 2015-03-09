3c79f6cce1f4af0c2266f0475efb549bed225b73
Jack O'Connor <jackocnr@gmail.com>
2015-03-09 15:04:34 -0700

    Create component.js

---
+++ component.js
@@ -0,0 +1,11 @@
+{
+  "name": "intl-tel-input",
+  "repo": "Bluefieldscom/intl-tel-input",
+  "version": "5.7.0",
+  "scripts": [ "build/js/intlTelInput.min.js" ],
+  "styles": [ "build/css/intlTelInput.css" ],
+  "dependencies": {
+    "jquery": ">=1.7"
+  },
+  "license": "MIT"
+}
