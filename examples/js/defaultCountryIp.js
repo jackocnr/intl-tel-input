f46bb7e440724bb9fd02161ae64171b7102ae229
Jack OConnor <jackocnr@gmail.com>
2015-06-05 10:36:27 +0100

    updated defaultCountryIp.js to use function

--- examples/js/defaultCountryIp.js
+++ examples/js/defaultCountryIp.js
@@ -1,4 +1,10 @@
 $("#phone").intlTelInput({
   defaultCountry: "auto",
+  geoIpLookup: function(callback) {
+    $.get('http://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+      var countryCode = (resp && resp.country) ? resp.country : "";
+      callback(countryCode);
+    });
+  },
   utilsScript: "../../lib/libphonenumber/build/utils.js" // just for formatting/placeholders etc
 });
