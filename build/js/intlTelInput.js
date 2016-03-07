d2523fa5cc9950e2744c9d9dddb42730ff0ae429
Jack OConnor <jackocnr@gmail.com>
2016-03-06 19:03:34 -0800

    Release v8.4.6

--- build/js/intlTelInput.js
+++ build/js/intlTelInput.js
@@ -1,5 +1,5 @@
 /*
- * International Telephone Input v8.4.5
+ * International Telephone Input v8.4.6
  * https://github.com/jackocnr/intl-tel-input.git
  * Licensed under the MIT license
  */
@@ -1048,7 +1048,7 @@
         }
     };
     // version
-    $.fn[pluginName].version = "8.4.5";
+    $.fn[pluginName].version = "8.4.6";
     // Tell JSHint to ignore this warning: "character may get silently deleted by one or more browsers"
     // jshint -W100
     // Array of country objects for the flag dropdown.
