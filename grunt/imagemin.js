cc174a40c8815d7861f3b08846052c268069dfed
Jonny Schmid <hi@jonathanschmid.de>
2016-05-04 19:01:53 +0100

    Remove imagemin-optipng reference

--- grunt/imagemin.js
+++ grunt/imagemin.js
@@ -1,18 +1,15 @@
-const imageminOptipng = require('imagemin-optipng');
-
 module.exports = function(grunt) {
   return {
     dynamic: {
       options: {
         optimizationLevel: 7,
-        use: [imageminOptipng()]
       },
       files: [{
         expand: true,
         cwd: 'build/img',
         src: ['**/*.{png,jpg,gif}'],
-        dest: 'build/img'
-      }]
-    }
+        dest: 'build/img',
+      }],
+    },
   };
 };
