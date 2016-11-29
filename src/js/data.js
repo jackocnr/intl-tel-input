7ccef6addb9db1d71dee9486b88222aa108f6a98
Jack OConnor <jackocnr@gmail.com>
2016-11-28 19:28:33 -0800

    update data.js comments

--- src/js/data.js
+++ src/js/data.js
@@ -1,42 +1,18 @@
 // Array of country objects for the flag dropdown.
-// Each contains a name, country code (ISO 3166-1 alpha-2) and dial code.
 
-// Originally from https://github.com/mledoze/countries
-// then with a couple of manual re-arrangements to be alphabetical
-// then changed Kazakhstan from +76 to +7
-// and Vatican City from +379 to +39 (see issue 50)
-// and Caribean Netherlands from +5997 to +599
-// and Curacao from +5999 to +599
-// Removed:  Kosovo, Pitcairn Islands, South Georgia
+// Here is the criteria for the plugin to support a given country/territory
+// - It has an iso2 code: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+// - It has a country calling code: https://en.wikipedia.org/wiki/List_of_country_calling_codes
+// - It has a flag in the region-flags project: https://github.com/behdad/region-flags/tree/gh-pages/png
+// - It is supported by libphonenumber (it must be listed here): https://github.com/googlei18n/libphonenumber/blob/master/resources/ShortNumberMetadata.xml
 
-// UPDATE Sept 12th 2015
-// List of regions that have iso2 country codes, which I have chosen to omit:
-// (based on this information: https://en.wikipedia.org/wiki/List_of_country_calling_codes)
-// AQ - Antarctica - all different country codes depending on which "base"
-// BV - Bouvet Island - no calling code
-// GS - South Georgia and the South Sandwich Islands - "inhospitable collection of islands" - same flag and calling code as Falkland Islands
-// HM - Heard Island and McDonald Islands - no calling code
-// PN - Pitcairn - tiny population (56), same calling code as New Zealand
-// TF - French Southern Territories - no calling code
-// UM - United States Minor Outlying Islands - no calling code
-
-// UPDATE the criteria of supported countries or territories (see issue 297)
-// Have an iso2 code: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
-// Have a country calling code: https://en.wikipedia.org/wiki/List_of_country_calling_codes
-// Have a flag
-// Must be supported by libphonenumber: https://github.com/googlei18n/libphonenumber
-
-// Update: converted objects to arrays to save bytes!
-// Update: added "priority" for countries with the same dialCode as others
-// Update: added array of area codes for countries with the same dialCode as others
-
-// So each country array has the following information:
+// Each country array has the following information:
 // [
 //    Country name,
 //    iso2 code,
 //    International dial code,
 //    Order (if >1 country with same dial code),
-//    Area codes (if >1 country with same dial code)
+//    Area codes
 // ]
 var allCountries = [
   [
