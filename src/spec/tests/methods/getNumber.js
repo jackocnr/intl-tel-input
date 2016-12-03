6d21058cc83a787c1b8fd0506ac2487fe0045211
Jack OConnor <jackocnr@gmail.com>
2016-12-02 21:06:29 -0800

    add extra getNumber test

--- src/spec/tests/methods/getNumber.js
+++ src/spec/tests/methods/getNumber.js
@@ -1,28 +1,62 @@
 "use strict";
 
-describe("getNumber: init plugin with valid US number and utils.js", function() {
+describe("getNumber: ", function() {
 
-  beforeEach(function() {
-    intlSetup(true);
-    input = $("<input value='+17024181234'>");
-    input.intlTelInput();
+  describe("initialising plugin with valid US number and utils.js", function() {
+
+    beforeEach(function() {
+      intlSetup(true);
+      input = $("<input value='+17024181234'>");
+      input.intlTelInput();
+    });
+
+    afterEach(function() {
+      input.intlTelInput("destroy");
+      input = null;
+    });
+
+    it("calling getNumber with no args returns the number as E.164", function() {
+      expect(input.intlTelInput("getNumber")).toEqual("+17024181234");
+    });
+
+    it("calling getNumber with format=INTERNATIONAL", function() {
+      expect(input.intlTelInput("getNumber", intlTelInputUtils.numberFormat.INTERNATIONAL)).toEqual("+1 702-418-1234");
+    });
+
+    it("calling getNumber with format=NATIONAL", function() {
+      expect(input.intlTelInput("getNumber", intlTelInputUtils.numberFormat.NATIONAL)).toEqual("(702) 418-1234");
+    });
+
   });
 
-  afterEach(function() {
-    input.intlTelInput("destroy");
-    input = null;
-  });
 
-  it("calling getNumber with no args returns the number as E.164", function() {
-    expect(input.intlTelInput("getNumber")).toEqual("+17024181234");
-  });
 
-  it("calling getNumber with format=INTERNATIONAL", function() {
-    expect(input.intlTelInput("getNumber", intlTelInputUtils.numberFormat.INTERNATIONAL)).toEqual("+1 702-418-1234");
-  });
+  describe("initialising plugin with utils.js", function() {
 
-  it("calling getNumber with format=NATIONAL", function() {
-    expect(input.intlTelInput("getNumber", intlTelInputUtils.numberFormat.NATIONAL)).toEqual("(702) 418-1234");
+    beforeEach(function() {
+      intlSetup(true);
+      input = $("<input>");
+      input.intlTelInput();
+    });
+
+    afterEach(function() {
+      input.intlTelInput("destroy");
+      input = null;
+    });
+
+    describe("selecting American Samoa and then typing a national number", function() {
+
+      beforeEach(function() {
+        selectFlag("as");
+        input.val("9785585898").keyup();
+      });
+
+      it("getNumber returns the correct number (with full dialcode/area code)", function() {
+        expect(input.intlTelInput("getNumber")).toEqual("+16849785585898");
+      });
+
+    });
+
   });
 
 });
