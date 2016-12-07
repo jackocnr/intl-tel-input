ab624ffd610776f13ab005c1ceed4c6c79e5a324
Jack OConnor <jackocnr@gmail.com>
2016-12-07 09:14:59 -0800

    Release v9.2.7

--- build/js/intlTelInput.js
+++ build/js/intlTelInput.js
@@ -1,5 +1,5 @@
 /*
- * International Telephone Input v9.2.6
+ * International Telephone Input v9.2.7
  * https://github.com/jackocnr/intl-tel-input.git
  * Licensed under the MIT license
  */
@@ -1067,7 +1067,7 @@
         }
     };
     // version
-    $.fn[pluginName].version = "9.2.6";
+    $.fn[pluginName].version = "9.2.7";
     // default options
     $.fn[pluginName].defaults = defaults;
     // Array of country objects for the flag dropdown.
