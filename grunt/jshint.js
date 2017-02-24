f2b668e1fe34d776182086140c015bb56db14e2e
Jack OConnor <jackocnr@gmail.com>
2017-02-24 10:44:10 -0800

    Release v11.0.4

--- grunt/jshint.js
+++ grunt/jshint.js
@@ -4,7 +4,8 @@ module.exports = function(grunt) {
     build: "tmp/wrapped.js",
     options: {
       // this is for data.js to avoid errors about (foreign) characters getting silently deleted by some browsers
-      '-W100': true
+      '-W100': true,
+      'eqnull': true
     }
   };
 };
