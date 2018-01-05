97e5903615598eca7612f4376268a0d0e586ac4f
Morne lotze <morne.lotze@getsmarter.com>
2018-01-05 13:26:52 +0200

    Added id's and aria properties

--- build/js/intlTelInput.js
+++ build/js/intlTelInput.js
@@ -204,7 +204,9 @@
             }).insertBefore(this.telInput);
             // currently selected flag (displayed to left of input)
             var selectedFlag = $("<div>", {
-                "class": "selected-flag"
+                "class": "selected-flag",
+                role: "combobox",
+                "aria-owns": "country-listbox"
             });
             selectedFlag.appendTo(this.flagsContainer);
             this.selectedFlagInner = $("<div>", {
@@ -224,12 +226,17 @@
                 }).appendTo(selectedFlag);
                 // country dropdown: preferred countries, then divider, then all countries
                 this.countryList = $("<ul>", {
-                    "class": "country-list hide"
+                    "class": "country-list hide",
+                    id: "country-listbox",
+                    "aria-expanded": "false",
+                    role: "listbox"
                 });
                 if (this.preferredCountries.length) {
                     this._appendListItems(this.preferredCountries, "preferred");
                     $("<li>", {
-                        "class": "divider"
+                        "class": "divider",
+                        role: "separator",
+                        "aria-disabled": "true"
                     }).appendTo(this.countryList);
                 }
                 this._appendListItems(this.countries, "");
@@ -263,7 +270,7 @@
             for (var i = 0; i < countries.length; i++) {
                 var c = countries[i];
                 // open the list item
-                tmp += "<li class='country " + className + "' data-dial-code='" + c.dialCode + "' data-country-code='" + c.iso2 + "'>";
+                tmp += "<li tabindex='0' class='country " + className + "' id='" + c.dialCode + "-" + c.iso2 + "' role='option' data-dial-code='" + c.dialCode + "' data-country-code='" + c.iso2 + "'>";
                 // add the flag
                 tmp += "<div class='flag-box'><div class='iti-flag " + c.iso2 + "'></div></div>";
                 // and the country name and dial code
@@ -526,7 +533,7 @@
                 this.dropdown.appendTo(this.options.dropdownContainer);
             }
             // show the menu and grab the dropdown height
-            this.dropdownHeight = this.countryList.removeClass("hide").outerHeight();
+            this.dropdownHeight = this.countryList.removeClass("hide").attr("aria-expanded", "true").outerHeight();
             if (!this.isMobile) {
                 var pos = this.telInput.offset(), inputTop = pos.top, windowTop = $(window).scrollTop(), // dropdownFitsBelow = (dropdownBottom < windowBottom)
                 dropdownFitsBelow = inputTop + this.telInput.outerHeight() + this.dropdownHeight < windowTop + $(window).height(), dropdownFitsAbove = inputTop - this.dropdownHeight > windowTop;
@@ -705,6 +712,10 @@
         _highlightListItem: function(listItem) {
             this.countryListItems.removeClass("highlight");
             listItem.addClass("highlight");
+            this.countryListItems.attr("aria-selected", "false");
+            listItem.attr("aria-selected", "true");
+            this.countryList.attr("aria-activedescendant", listItem.attr("id"));
+            listItem.focus();
         },
         // find the country data for the given country code
         // the ignoreOnlyCountriesOption is only used during init() while parsing the onlyCountries array
@@ -787,6 +798,7 @@
         // close the dropdown and unbind any listeners
         _closeDropdown: function() {
             this.countryList.addClass("hide");
+            this.countryList.attr("aria-expanded", "false");
             // update the arrow
             this.selectedFlagInner.children(".iti-arrow").removeClass("up");
             // unbind key events
