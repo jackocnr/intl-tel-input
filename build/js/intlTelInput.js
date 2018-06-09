28c1e6bd1bc8b8f5f254cc005aa4d0f86ca95655
Jack OConnor <jackocnr@gmail.com>
2018-06-09 10:47:46 +0100

    Release v12.1.16

--- build/js/intlTelInput.js
+++ build/js/intlTelInput.js
@@ -1,5 +1,5 @@
 /*
- * International Telephone Input v12.1.15
+ * International Telephone Input v12.1.16
  * https://github.com/jackocnr/intl-tel-input.git
  * Licensed under the MIT license
  */
@@ -1112,7 +1112,7 @@
     // default options
     $.fn[pluginName].defaults = defaults;
     // version
-    $.fn[pluginName].version = "12.1.15";
+    $.fn[pluginName].version = "12.1.16";
     // Array of country objects for the flag dropdown.
     // Here is the criteria for the plugin to support a given country/territory
     // - It has an iso2 code: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
