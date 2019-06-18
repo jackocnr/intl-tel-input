9970eab84cc738976bcb5f41b119b38e97111906
Jack OConnor <jackocnr@gmail.com>
2019-06-18 21:20:56 +0100

    NANP countries should just have +1 as dial code

--- src/js/data.js
+++ src/js/data.js
@@ -33,7 +33,9 @@ var allCountries = [
   [
     "American Samoa",
     "as",
-    "1684"
+    "1",
+    5,
+    ["684"]
   ],
   [
     "Andorra",
@@ -48,12 +50,16 @@ var allCountries = [
   [
     "Anguilla",
     "ai",
-    "1264"
+    "1",
+    6,
+    ["264"]
   ],
   [
     "Antigua and Barbuda",
     "ag",
-    "1268"
+    "1",
+    7,
+    ["268"]
   ],
   [
     "Argentina",
@@ -89,7 +95,9 @@ var allCountries = [
   [
     "Bahamas",
     "bs",
-    "1242"
+    "1",
+    8,
+    ["242"]
   ],
   [
     "Bahrain (‫البحرين‬‎)",
@@ -104,7 +112,9 @@ var allCountries = [
   [
     "Barbados",
     "bb",
-    "1246"
+    "1",
+    9,
+    ["246"]
   ],
   [
     "Belarus (Беларусь)",
@@ -129,7 +139,9 @@ var allCountries = [
   [
     "Bermuda",
     "bm",
-    "1441"
+    "1",
+    10,
+    ["441"]
   ],
   [
     "Bhutan (འབྲུག)",
@@ -164,7 +176,9 @@ var allCountries = [
   [
     "British Virgin Islands",
     "vg",
-    "1284"
+    "1",
+    11,
+    ["284"]
   ],
   [
     "Brunei",
@@ -218,7 +232,9 @@ var allCountries = [
   [
     "Cayman Islands",
     "ky",
-    "1345"
+    "1",
+    12,
+    ["345"]
   ],
   [
     "Central African Republic (République centrafricaine)",
@@ -326,7 +342,9 @@ var allCountries = [
   [
     "Dominica",
     "dm",
-    "1767"
+    "1",
+    13,
+    ["767"]
   ],
   [
     "Dominican Republic (República Dominicana)",
@@ -449,7 +467,9 @@ var allCountries = [
   [
     "Grenada",
     "gd",
-    "1473"
+    "1",
+    14,
+    ["473"]
   ],
   [
     "Guadeloupe",
@@ -460,7 +480,9 @@ var allCountries = [
   [
     "Guam",
     "gu",
-    "1671"
+    "1",
+    15,
+    ["671"]
   ],
   [
     "Guatemala",
@@ -758,7 +780,9 @@ var allCountries = [
   [
     "Montserrat",
     "ms",
-    "1664"
+    "1",
+    16,
+    ["664"]
   ],
   [
     "Morocco (‫المغرب‬‎)",
@@ -839,7 +863,9 @@ var allCountries = [
   [
     "Northern Mariana Islands",
     "mp",
-    "1670"
+    "1",
+    17,
+    ["670"]
   ],
   [
     "Norway (Norge)",
@@ -950,12 +976,16 @@ var allCountries = [
   [
     "Saint Kitts and Nevis",
     "kn",
-    "1869"
+    "1",
+    18,
+    ["869"]
   ],
   [
     "Saint Lucia",
     "lc",
-    "1758"
+    "1",
+    19,
+    ["758"]
   ],
   [
     "Saint Martin (Saint-Martin (partie française))",
@@ -971,7 +1001,9 @@ var allCountries = [
   [
     "Saint Vincent and the Grenadines",
     "vc",
-    "1784"
+    "1",
+    20,
+    ["784"]
   ],
   [
     "Samoa",
@@ -1021,7 +1053,9 @@ var allCountries = [
   [
     "Sint Maarten",
     "sx",
-    "1721"
+    "1",
+    21,
+    ["721"]
   ],
   [
     "Slovakia (Slovensko)",
@@ -1148,7 +1182,9 @@ var allCountries = [
   [
     "Trinidad and Tobago",
     "tt",
-    "1868"
+    "1",
+    22,
+    ["868"]
   ],
   [
     "Tunisia (‫تونس‬‎)",
@@ -1168,7 +1204,9 @@ var allCountries = [
   [
     "Turks and Caicos Islands",
     "tc",
-    "1649"
+    "1",
+    23,
+    ["649"]
   ],
   [
     "Tuvalu",
@@ -1178,7 +1216,9 @@ var allCountries = [
   [
     "U.S. Virgin Islands",
     "vi",
-    "1340"
+    "1",
+    24,
+    ["340"]
   ],
   [
     "Uganda",
