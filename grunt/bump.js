a66d729a33bd1f0a7ae577144cf10bb2464f19f0
Jack OConnor <jackocnr@gmail.com>
2020-04-03 10:16:01 +0100

    Update bump.js

--- grunt/bump.js
+++ grunt/bump.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
   return {
     options: {
-      files: ['package.json', 'intl-tel-input.jquery.json', 'component.json', 'composer.json'],
+      files: ['package.json', 'package-lock.json', 'composer.json'],
       updateConfigs: ['package'],
       commitFiles: ['-a'],
       pushTo: 'origin'
