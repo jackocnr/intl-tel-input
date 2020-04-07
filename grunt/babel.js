4f09ffeed5ca854f3810ae494bb4c38a39c435fd
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2020-04-07 11:53:23 -0700

    Disable transform-typeof-symbol Babel plugin
    Based on this conversation: https://github.com/jackocnr/intl-tel-input/pull/1057#issuecomment-610530882

--- grunt/babel.js
+++ grunt/babel.js
@@ -2,7 +2,14 @@ module.exports = function(grunt) {
   return {
     options: {
       sourceMap: false,
-      presets: ['@babel/preset-env']
+      presets: [
+        ['@babel/preset-env', {
+          // we're not performing any typeof checks in this project where it's important to know
+          // if an object is a symbol. Let the consumer of this package compile with this option
+          // only if he wants it.
+          exclude: ['transform-typeof-symbol']
+        }]
+      ]
     },
     dist: {
       files: {
