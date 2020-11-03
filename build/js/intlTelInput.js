61b3d5180e0ef1f37131a7c713a89fe9a1b316da
Tim Breitkreutz <tim.breitkreutz@drip.com>
2020-11-03 11:07:25 -0500

    remove a comment

--- build/js/intlTelInput.js
+++ build/js/intlTelInput.js
@@ -7,8 +7,6 @@
 // wrap in UMD
 (function(factory) {
     // Detect if another instance has already been loaded on this page
-    // Another option would be to place the new global object
-    // inside the main one in an array perhaps.
     var instanceGlobalKey = "intlTelInputGlobals";
     for (var ii = 1; window[instanceGlobalKey] !== undefined; ii += 1) {
         instanceGlobalKey = "intlTelInputGlobals" + ii;
