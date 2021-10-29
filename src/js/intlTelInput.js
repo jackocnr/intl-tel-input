c2052fbad254a5637fc0ad004679087e38ed7f5d
Federico <federico@anymindgroup.com>
2021-10-29 15:31:02 +0900

    Issue #1266

--- src/js/intlTelInput.js
+++ src/js/intlTelInput.js
@@ -331,11 +331,13 @@ class Iti {
     // selected flag (displayed to left of input)
     this.selectedFlag = this._createEl('div', {
       class: 'iti__selected-flag',
-      role: 'combobox',
-      'aria-controls': `iti-${this.id}__country-listbox`,
-      'aria-owns': `iti-${this.id}__country-listbox`,
-      'aria-expanded': 'false',
-      'aria-label': 'Telephone country code',
+      ...(this.options.allowDropdown && {
+        role: 'combobox',
+        'aria-controls': `iti-${this.id}__country-listbox`,
+        'aria-owns': `iti-${this.id}__country-listbox`,
+        'aria-expanded': 'false',
+        'aria-label': 'Telephone country code',
+      }),
     }, this.flagsContainer);
     this.selectedFlagInner = this._createEl('div', { class: 'iti__flag' }, this.selectedFlag);
 
