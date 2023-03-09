a628ea3ecce650b441e2857dd3db79187e3ad426
Jack OConnor <jackocnr@gmail.com>
2023-03-09 17:56:32 +0000

    Release v17.0.20

--- build/js/intlTelInput.js
+++ build/js/intlTelInput.js
@@ -1,5 +1,5 @@
 /*
- * International Telephone Input v17.0.19
+ * International Telephone Input v17.0.20
  * https://github.com/jackocnr/intl-tel-input.git
  * Licensed under the MIT license
  */
@@ -1340,7 +1340,7 @@
         // default options
         intlTelInputGlobals.defaults = defaults;
         // version
-        intlTelInputGlobals.version = "17.0.19";
+        intlTelInputGlobals.version = "17.0.20";
         // convenience wrapper
         return function(input, options) {
             var iti = new Iti(input, options);
