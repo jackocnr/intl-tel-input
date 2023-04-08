d0a4e25e83c6030d881d9e4ecbb57a704adc81c9
Jack OConnor <jackocnr@gmail.com>
2023-04-08 11:41:43 +0100

    Update examples

--- examples/gen/js/initPromise.js
+++ examples/gen/js/initPromise.js
@@ -2,7 +2,7 @@ var input = document.querySelector("#phone");
 var statusElement = document.querySelector("#status");
 
 var iti = window.intlTelInput(input, {
-  utilsScript: "../../build/js/utils.js?1680950450342",
+  utilsScript: "../../build/js/utils.js?1680950494824",
 });
 iti.promise.then(function() {
   statusElement.innerHTML = "Initialised!";
