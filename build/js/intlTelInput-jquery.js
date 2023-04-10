bccdb6c06c9d252c5c9c7dda839732aee9b9ce8c
Jack OConnor <jackocnr@gmail.com>
2023-04-10 10:33:12 +0100

    Update version to v18.1.0

--- build/js/intlTelInput-jquery.js
+++ build/js/intlTelInput-jquery.js
@@ -1,5 +1,5 @@
 /*
- * International Telephone Input v18.0.3
+ * International Telephone Input v18.1.0
  * https://github.com/jackocnr/intl-tel-input.git
  * Licensed under the MIT license
  */
@@ -1416,7 +1416,7 @@
     // default options
     intlTelInputGlobals.defaults = defaults;
     // version
-    intlTelInputGlobals.version = "18.0.3";
+    intlTelInputGlobals.version = "18.1.0";
     var pluginName = "intlTelInput";
     // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
--- build/js/intlTelInput.js
+++ build/js/intlTelInput.js
@@ -1,5 +1,5 @@
 /*
- * International Telephone Input v18.0.3
+ * International Telephone Input v18.1.0
  * https://github.com/jackocnr/intl-tel-input.git
  * Licensed under the MIT license
  */
@@ -1411,7 +1411,7 @@
         // default options
         intlTelInputGlobals.defaults = defaults;
         // version
-        intlTelInputGlobals.version = "18.0.3";
+        intlTelInputGlobals.version = "18.1.0";
         // convenience wrapper
         return function(input, options) {
             var iti = new Iti(input, options);
