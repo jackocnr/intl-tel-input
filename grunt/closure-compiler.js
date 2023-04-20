c5b4619c9a020a469a6047ccb14d8396e2f51f90
Nam PHAM <13-npham@users.noreply.gitlab.maiia.io>
2023-04-20 11:41:20 +0200

    ignore demo-compiled

--- grunt/closure-compiler.js
+++ grunt/closure-compiler.js
@@ -8,6 +8,7 @@ module.exports = function (grunt) {
         js: [
           "node_modules/google-closure-library/**.js",
           "third_party/libphonenumber/javascript/i18n/phonenumbers/**.js",
+          "!third_party/libphonenumber/javascript/i18n/phonenumbers/demo-compiled.js",
           "!third_party/libphonenumber/javascript/i18n/phonenumbers/metadatafortesting.js",
           "!third_party/libphonenumber/javascript/i18n/phonenumbers/metadatalite.js",
           "!third_party/libphonenumber/javascript/i18n/phonenumbers/regioncodefortesting.js",
@@ -16,7 +17,7 @@ module.exports = function (grunt) {
         entry_point: "goog:i18n.phonenumbers.demo",
         manage_closure_dependencies: true,
         compilation_level: "ADVANCED_OPTIMIZATIONS",
-        output_wrapper: "(function(){%output%})();"
+        output_wrapper: "(function(){%output%})();",
       },
     },
   };
