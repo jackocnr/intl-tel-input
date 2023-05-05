01a4529ce4cc0a830b2af09da8c5376cc7e5245f
Danny Kirschner <danny@cityinnovate.com>
2023-05-05 10:11:28 -0400

    1415: flag container title attribute updates
    - when separateDialCode is enabled, removes dial code from title
    
    closes https://github.com/jackocnr/intl-tel-input/issues/1415

--- src/spec/tests/methods/setCountry.js
+++ src/spec/tests/methods/setCountry.js
@@ -23,4 +23,48 @@ describe("setCountry: init plugin and calling public method setCountry()", funct
     expect(getInputVal()).toEqual("");
   });
 
+  describe("setting title attribute on flag container", function() {
+    describe("when separateDialCode is false", function() {
+      beforeEach(function() {
+        iti = window.intlTelInput(input[0], {
+          showFlags: true,
+          separateDialCode: false,
+        });
+        iti.setCountry(countryCode);
+      });
+
+      it("has the country name and dial code in the flag's title", function() {
+        expect(getSelectedFlagContainer().attr("title")).toEqual("United Kingdom: +44");
+      });
+    });
+
+    describe("when separateDialCode is true", function() {
+      beforeEach(function() {
+        iti = window.intlTelInput(input[0], {
+          showFlags: true,
+          separateDialCode: true,
+        });
+        iti.setCountry(countryCode);
+      });
+
+      it("has the country name but not the dial code in the flag's title", function() {
+        expect(getSelectedFlagContainer().attr("title")).toEqual("United Kingdom");
+      });
+    });
+
+
+    // setCountry errors out when country code is undefined, so we can likely remove this scenario and make country code a required param
+    describe("when countryCode is falsey", function() {
+      // beforeEach(function() {
+      //   iti = window.intlTelInput(input[0], {
+      //     showFlags: true,
+      //   });
+      //   iti.setCountry();
+      // });
+
+      it("sets the flag's title as Unknown", function() {
+        // expect(getSelectedFlagContainer().attr("title")).toEqual("Unknown");
+      });
+    })
+  });
 });
