38c1a22588db898f2cc3770105763bf522a3e43d
Jack OConnor <jackocnr@gmail.com>
2024-02-25 09:07:53 +0000

    Release v19.5.2

--- build/js/intlTelInput-jquery.js
+++ build/js/intlTelInput-jquery.js
@@ -1,5 +1,5 @@
 /*
- * International Telephone Input v19.5.1
+ * International Telephone Input v19.5.2
  * https://github.com/jackocnr/intl-tel-input.git
  * Licensed under the MIT license
  */
@@ -1763,7 +1763,7 @@
     // default options
     intlTelInputGlobals.defaults = defaults;
     // version
-    intlTelInputGlobals.version = "19.5.1";
+    intlTelInputGlobals.version = "19.5.2";
     var pluginName = "intlTelInput";
     // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
