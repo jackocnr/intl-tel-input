8722677244dba7afb46e8261c4f68ec73c1818bb
Anthony Veaudry <anthony@veaudry.pro>
2024-04-08 11:56:48 +0300

    Clean up remnant.
    This code was to generate the original sprite and css.

--- grunt/sprite.js
+++ grunt/sprite.js
@@ -3,29 +3,23 @@ module.exports = function(grunt) {
     retina: {
       src: 'src/img/flags/@2x/*.png',
       dest: 'build/img/flags@2x.png',
-      destCss: 'tmp/sprite@2x.scss', //* Ignore - we just use the @1x styles for both.
-      padding: 4,
+      destCss: 'tmp/sprite@2x.scss', //! Ignore - We are manually generating the styles.
+      padding: 4, //* This needs to be double the main padding so the math in the css files work.
       algorithm: 'left-right',
       algorithmOpts: {
         sort: false
       },
-      cssOpts: {
-        variableNameTransforms: ['toLowerCase']
-      }
     },
     main: {
       src: ['src/img/flags/@1x/*.png'],
       dest: 'build/img/flags.png',
       cssTemplate: 'grunt/tmpl/sprite.scss.mustache',
-      destCss: 'src/css/sprite.scss',
+      destCss: 'tmp/sprite.scss', //! Ignore - We are manually generating the styles.
       padding: 2, //* This is currently just for chrome, otherwise flags seem to leak into each other.
       algorithm: 'left-right',
       algorithmOpts: {
         sort: false
       },
-      cssOpts: {
-        variableNameTransforms: ['toLowerCase']
-      }
     }
   };
 };
