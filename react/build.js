156ca7e1ea812f2b636367d7ec48e84ec84c4945
Jack OConnor <jackocnr@gmail.com>
2024-04-10 14:59:42 +0100

    Expose react CJS module

--- react/build.js
+++ react/build.js
@@ -3,30 +3,43 @@
 const { build } = require("esbuild");
 const packageJson = require("../package.json");
 
-build({
+const mainShared = {
   bundle: true,
   entryPoints: ["react/src/intl-tel-input/react.tsx"],
   external: ["react", "react-dom", "prop-types"],
   logLevel: "info",
   minify: true,
   define: { "process.env.VERSION": `"${packageJson.version}"` },
+};
+
+//* React Component - CommonJS
+build({
+  ...mainShared,
+  format: "cjs",
+  outfile: "react/build/IntlTelInput.cjs",
+});
+
+//* React Component - Default (ES Modules)
+build({
+  ...mainShared,
   format: "esm",
   outfile: "react/build/IntlTelInput.js",
 });
 
-//* Demo Files
 const demoShared = {
   bundle: true,
   define: { "process.env.VERSION": `"${packageJson.version}"` },
   format: "iife",
 };
 
+//* Simple demo app
 build({
   ...demoShared,
   entryPoints: ["react/demo/SimpleApp.tsx"],
   outfile: "react/demo/simple-bundle.js",
 });
 
+//* Validation demo app
 build({
   ...demoShared,
   entryPoints: ["react/demo/ValidationApp.tsx"],
