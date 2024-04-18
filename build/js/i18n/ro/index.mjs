7534b922b3e88b5f248d8ee1bf6c2edd848e5523
Anthony Veaudry <anthony@veaudry.pro>
2024-04-18 11:46:48 +0300

    Updates build

--- build/js/i18n/ro/index.mjs
+++ build/js/i18n/ro/index.mjs
@@ -1,4 +1,7 @@
 import countryTranslations from "./countries.mjs";
 import interfaceTranslations from "./interface.mjs";
 
+export { default as countryTranslations } from "./countries.mjs";
+export { default as interfaceTranslations } from "./interface.mjs";
+
 export default { ...countryTranslations, ...interfaceTranslations };
