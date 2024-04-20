77c0c4fbc395aa213b95988d76f8cba5d5c62a0c
Jack OConnor <jackocnr@gmail.com>
2024-04-21 00:07:36 +0100

    Release v21.2.5

--- build/js/intlTelInput.js
+++ build/js/intlTelInput.js
@@ -1,5 +1,5 @@
 /*
- * International Telephone Input v21.2.4
+ * International Telephone Input v21.2.5
  * https://github.com/jackocnr/intl-tel-input.git
  * Licensed under the MIT license
  */
@@ -2883,7 +2883,7 @@ var factoryOutput = (() => {
       //* A map from instance ID to instance object.
       instances: {},
       loadUtils,
-      version: "21.2.4"
+      version: "21.2.5"
     };
     window.intlTelInputGlobals = intlTelInputGlobals;
   }
--- react/demo/simple-bundle.js
+++ react/demo/simple-bundle.js
@@ -26358,7 +26358,7 @@
       //* A map from instance ID to instance object.
       instances: {},
       loadUtils,
-      version: "21.2.4"
+      version: "21.2.5"
     };
     window.intlTelInputGlobals = intlTelInputGlobals;
   }
