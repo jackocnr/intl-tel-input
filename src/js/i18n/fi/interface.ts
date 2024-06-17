a45369e331c3b1247682319ca3d5f30f9e4ccb7a
Michael Winton <wintonmr@gmail.com>
2024-06-17 15:42:55 +0200

    Add Finnish translations

---
+++ src/js/i18n/fi/interface.ts
@@ -0,0 +1,14 @@
+//* Finnish. Translated by: Michael Winton (mrwinton).
+export default {
+  "selectedCountryAriaLabel": "Valittu maa",
+  "noCountrySelected": "Maata ei ole valittu",
+  "countryListAriaLabel": "Luettelo maista",
+  "searchPlaceholder": "Haku",
+  "zeroSearchResults": "Ei tuloksia",
+  "oneSearchResult": "1 tulos löytyi",
+  "multipleSearchResults": "${count} tulosta löytyi",
+
+  // additional countries (not supported by country-list library)
+  "ac": "Ascension",
+  "xk": "Kosovo",
+};
