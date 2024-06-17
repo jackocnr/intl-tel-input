8a27ecced3f989499981946a8122ceb6eeb829e9
Michael Winton <wintonmr@gmail.com>
2024-06-17 15:42:04 +0200

    Add Swedish translations

---
+++ build/js/i18n/sv/interface.js
@@ -0,0 +1,14 @@
+//* Swedish. Translated by: Nhi Tran.
+export default {
+  "selectedCountryAriaLabel": "Valt land",
+  "noCountrySelected": "Inget land valt",
+  "countryListAriaLabel": "Lista över länder",
+  "searchPlaceholder": "Sök",
+  "zeroSearchResults": "Inga resultat hittades",
+  "oneSearchResult": "1 resultat hittades",
+  "multipleSearchResults": "${count} resultat hittades",
+
+  // additional countries (not supported by country-list library)
+  "ac": "Ascension",
+  "xk": "Kosovo",
+};
