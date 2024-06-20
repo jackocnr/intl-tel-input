a7c94b5060c6568065ff89b28f2c9fa8ce7fce31
Jack OConnor <jackocnr@gmail.com>
2024-06-20 09:45:25 +0100

    Cleanup stale config

--- grunt/sprite.js
+++ grunt/sprite.js
@@ -13,7 +13,6 @@ module.exports = function(grunt) {
     main: {
       src: ['src/img/flags/@1x/*.png'],
       dest: 'build/img/flags.png',
-      cssTemplate: 'grunt/tmpl/sprite.scss.mustache',
       destCss: 'tmp/sprite.scss', //! Ignore - We are manually generating the styles.
       padding: 2, //* This is currently just for chrome, otherwise flags seem to leak into each other.
       algorithm: 'left-right',
