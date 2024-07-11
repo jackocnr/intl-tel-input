b634960065cc7555b353a6e40f742309546393cc
Jack OConnor <jackocnr@gmail.com>
2024-07-11 15:26:39 +0100

    Release v23.3.2

--- build/js/data.js
+++ build/js/data.js
@@ -1,5 +1,5 @@
 /*
- * International Telephone Input v23.3.1
+ * International Telephone Input v23.3.2
  * https://github.com/jackocnr/intl-tel-input.git
  * Licensed under the MIT license
  */
--- react/build/IntlTelInputWithUtils.cjs
+++ react/build/IntlTelInputWithUtils.cjs
@@ -2758,7 +2758,7 @@ var intlTelInput = Object.assign(
     //* A map from instance ID to instance object.
     instances: {},
     loadUtils,
-    version: "23.3.1"
+    version: "23.3.2"
   }
 );
 var intl_tel_input_default = intlTelInput;
