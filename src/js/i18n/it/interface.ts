e7cbadf29867abd6e5e1a946e65d948d48bcd539
Jan Hermann <hermajan@gmail.com>
2024-07-19 17:33:17 +0200

    Added CA, CS, IT, SK

---
+++ src/js/i18n/it/interface.ts
@@ -0,0 +1,14 @@
+//* Italian. Translated by: Google Translate.
+export default {
+  selectedCountryAriaLabel: "Paese selezionato",
+  noCountrySelected: "Nessun paese selezionato",
+  countryListAriaLabel: "Elenco dei paesi",
+  searchPlaceholder: "Ricerca",
+  zeroSearchResults: "Nessun risultato trovato",
+  oneSearchResult: "1 risultato trovato",
+  multipleSearchResults: "${count} risultati trovati",
+  
+  // additional countries (not supported by country-list library)
+  ac: "Isola di Ascensione",
+  xk: "Kosovo",
+};
