25b69d60d07d62b19c04747e89d14073ef7cfce0
bgb10 <bgb10@naver.com>
2024-08-18 15:05:51 -0700

    erase stale dependencies/files

--- grunt/exec.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(grunt) {
-  return {
-    evenizer: {
-      //* We need even pixel values for width and height before we scale the image down to @1x.
-      command: "evenizer --resize -i src/img/flags/@2x/*.png"
-    }
-  };
-};
