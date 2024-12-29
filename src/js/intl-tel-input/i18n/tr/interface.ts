c74464bf0b919fc601de01476324ad484c45beec
Hasan Özbey <48382593+hasan-ozbey@users.noreply.github.com>
2024-12-29 16:40:11 +0300

    Update interface.ts

--- src/js/intl-tel-input/i18n/tr/interface.ts
+++ src/js/intl-tel-input/i18n/tr/interface.ts
@@ -5,13 +5,13 @@ const interfaceTranslations: I18n = {
   selectedCountryAriaLabel: "Seçilen ülke",
   noCountrySelected: "Hiçbir ülke seçilmedi",
   countryListAriaLabel: "Ülke listesi",
-  searchPlaceholder: "Aramak",
+  searchPlaceholder: "Ara",
   zeroSearchResults: "Sonuç bulunamadı",
   oneSearchResult: "1 sonuç bulundu",
   multipleSearchResults: "${count} sonuç bulundu",
   
   // additional countries (not supported by country-list library)
-  ac: "Yükselme adası",
+  ac: "Ascension Adası",
   xk: "Kosova",
 };
 
