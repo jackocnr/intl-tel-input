10e1d2859f261341278eb9ec9367b78f0b58bdb7
Pawan Kumar <pwnkumar201@gmail.com>
2025-01-09 21:47:19 +0530

    Added container class in the country list fullscreen popup wrapper.

--- src/spec/tests/options/containerClass.js
+++ src/spec/tests/options/containerClass.js
@@ -26,4 +26,20 @@ describe("containerClass option:", function() {
 
   });
 
+  describe("init plugin with containerClass in popup", function() {
+
+    beforeEach(function() {
+      iti = window.intlTelInput(input[0], {
+		useFullscreenPopup: true,
+        containerClass: "cpc",
+      });
+    });
+
+    it("sets the classes properly", function() {
+      getSelectedCountryContainer().click();
+      //* After opening the dropdown, check the dropdown container have the right class.
+      expect($(".iti--container")).toHaveClass("cpc");
+    });
+  });
+
 });
