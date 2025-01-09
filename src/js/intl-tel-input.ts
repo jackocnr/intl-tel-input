5ca6fb89f2cfa12c0fceb1ee17cef63247ef4d8f
Pawan Kumar <pwnkumar201@gmail.com>
2025-01-09 21:47:19 +0530

    Added container class in the country list fullscreen popup wrapper.

--- src/js/intl-tel-input.ts
+++ src/js/intl-tel-input.ts
@@ -704,6 +704,9 @@ export class Iti {
         //* Create dropdownContainer markup.
         if (dropdownContainer) {
           let dropdownClasses = "iti iti--container";
+          if (containerClass) {
+            dropdownClasses += ` ${containerClass}`;
+          }
           if (useFullscreenPopup) {
             dropdownClasses += " iti--fullscreen-popup";
           } else {
