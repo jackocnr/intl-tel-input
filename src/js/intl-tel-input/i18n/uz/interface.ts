7d02c28561bab407f35477b317f0267ea0a0da0b
khusanov-m <khusanov.it.tech@gmail.com>
2025-05-11 08:25:11 +0500

    Add Uzbek (uz) translation

---
+++ src/js/intl-tel-input/i18n/uz/interface.ts
@@ -0,0 +1,18 @@
+//* English. Translated by: Mukhammadkhojiakbar Khusanov (khusanov-m).
+import { I18n } from "../types";
+
+const interfaceTranslations: I18n = {
+  selectedCountryAriaLabel: "Tanlangan davlat",
+  noCountrySelected: "Davlat tanlanmagan",
+  countryListAriaLabel: "Davlatlar roʻyxati",
+  searchPlaceholder: "Davlatni qidiring",
+  zeroSearchResults: "Natija topilmadi",
+  oneSearchResult: "1-ta natija topildi",
+  multipleSearchResults: "${count}-ta natija topildi",
+
+  // qo‘shimcha davlatlar (country-list kutubxonasi tomonidan qo‘llab-quvvatlanmaydi)
+  ac: "Ascension oroli",
+  xk: "Kosovo",
+};
+
+export default interfaceTranslations;
