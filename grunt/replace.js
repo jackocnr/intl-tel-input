d83c785a36f02e4f5aa18a0f3f73ef9c10896b16
Jack OConnor <jackocnr@gmail.com>
2025-08-26 13:20:54 +0100

    tweak readme version num

--- grunt/replace.js
+++ grunt/replace.js
@@ -266,7 +266,8 @@ module.exports = function(grunt) {
       files: {
         'README.md': 'README.md',
         'react/README.md': 'react/README.md',
-        'vue/README.md': 'vue/README.md'
+        'vue/README.md': 'vue/README.md',
+        'angular/README.md': 'angular/README.md'
       }
     },
     issueTemplate: {
