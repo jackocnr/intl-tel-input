c5e759ad468f978deebc1fdd842619745bb4097f
CorvS <w8.account@outlook.de>
2025-10-21 09:03:29 +0200

    Add Shqip (sq) translation

---
+++ src/js/intl-tel-input/i18n/sq/interface.ts
@@ -0,0 +1,19 @@
+//* Shqip (Albanian). Translated by: ChatGPT 5.
+import { I18n } from "../types";
+
+const interfaceTranslations: I18n = {
+  selectedCountryAriaLabel: "Ndrysho vendin, i zgjedhur ${countryName} (${dialCode})",
+  noCountrySelected: "Zgjidh vendin",
+  countryListAriaLabel: "Lista e vendeve",
+  searchPlaceholder: "Kërko",
+  clearSearchAriaLabel: "Pastro kërkimin",
+  zeroSearchResults: "Nuk u gjet asnjë rezultat",
+  oneSearchResult: "U gjet 1 rezultat",
+  multipleSearchResults: "U gjetën ${count} rezultate",
+
+  // additional countries (not supported by country-list library)
+  ac: "Ishulli Ascension",
+  xk: "Kosova",
+};
+
+export default interfaceTranslations;
