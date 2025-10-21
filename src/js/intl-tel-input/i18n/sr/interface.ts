752df9d4a722526531fd81a175934e45741f56e7
CorvS <w8.account@outlook.de>
2025-10-21 09:04:06 +0200

    Add Serbian (sr) translation

---
+++ src/js/intl-tel-input/i18n/sr/interface.ts
@@ -0,0 +1,19 @@
+//* Serbian. Translated by: ChatGPT 5.
+import { I18n } from "../types";
+
+const interfaceTranslations: I18n = {
+  selectedCountryAriaLabel: "Промени земљу, изабрано ${countryName} (${dialCode})",
+  noCountrySelected: "Изабери земљу",
+  countryListAriaLabel: "Листа земаља",
+  searchPlaceholder: "Претрага",
+  clearSearchAriaLabel: "Обриши претрагу",
+  zeroSearchResults: "Нема резултата",
+  oneSearchResult: "Пронађен 1 резултат",
+  multipleSearchResults: "Пронађено ${count} резултата",
+
+  // additional countries (not supported by country-list library)
+  ac: "Острво Асенсион",
+  xk: "Косово",
+};
+
+export default interfaceTranslations;
