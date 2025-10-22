a33fdfc5d7ffcaeef179c063c997e4753a9f0fe3
CorvS <w8.account@outlook.de>
2025-10-22 21:16:59 +0200

    Add alternative pluralisation logic for Lithuanian

--- src/js/intl-tel-input/i18n/lt/interface.ts
+++ src/js/intl-tel-input/i18n/lt/interface.ts
@@ -2,14 +2,34 @@
 import { I18n } from "../types";
 
 const interfaceTranslations: I18n = {
-  selectedCountryAriaLabel: "Pakeisti šalį, pasirinkta ${countryName} (${dialCode})",
+  selectedCountryAriaLabel:
+    "Pakeisti šalį, pasirinkta ${countryName} (${dialCode})",
   noCountrySelected: "Pasirinkite šalį",
   countryListAriaLabel: "Šalių sąrašas",
   searchPlaceholder: "Paieška",
   clearSearchAriaLabel: "Išvalyti paiešką",
   zeroSearchResults: "Rezultatų nerasta",
-  oneSearchResult: "Rastas 1 rezultatas",
-  multipleSearchResults: "Rasta ${count} rezultatų",
+
+  searchResultsText(count) {
+    if (count === 1) {
+      return "Rastas 1 rezultatas";
+    }
+
+    const mod10 = count % 10;
+    const mod100 = count % 100;
+
+    // Numbers ending in 1, but not 11
+    if (mod10 === 1 && mod100 !== 11) {
+      return `Rasti ${count} rezultatas`;
+    }
+
+    // Numbers ending in 2-9, but not 11-19
+    if (mod10 >= 2 && mod10 <= 9 && !(mod100 >= 11 && mod100 <= 19)) {
+      return `Rasti ${count} rezultatai`;
+    }
+
+    return `Rasta ${count} rezultatų`;
+  },
 
   // additional countries (not supported by country-list library)
   ac: "Dangun Žengimo sala",
