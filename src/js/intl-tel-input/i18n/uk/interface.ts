b2d078c3e5c7c2b78fbe4489aa01b54878acbe99
CorvS <w8.account@outlook.de>
2025-10-23 09:03:55 +0200

    Add alternative pluralisation logic for Ukrainian

--- src/js/intl-tel-input/i18n/uk/interface.ts
+++ src/js/intl-tel-input/i18n/uk/interface.ts
@@ -7,9 +7,22 @@ const interfaceTranslations: I18n = {
   countryListAriaLabel: "Список країн",
   searchPlaceholder: "Шукати",
   zeroSearchResults: "Результатів не знайдено",
-  oneSearchResult: "Знайдено 1 результат",
-  multipleSearchResults: "Знайдено ${count} результатів",
-  
+
+  searchResultsText(count) {
+    const mod10 = count % 10;
+    const mod100 = count % 100;
+
+    // Numbers ending in 1, but not 11
+    if (mod10 === 1 && mod100 !== 11) {
+      return `Знайдено ${count} результат`;
+    }
+
+    // Numbers ending in 2-4, but not 12-14
+    const isFew = mod10 >= 2 && mod10 <= 4 && !(mod100 >= 12 && mod100 <= 14);
+
+    return `Знайдено ${count} ${isFew ? "результати" : "результатів"}`;
+  },
+
   // additional countries (not supported by country-list library)
   ac: "Острів Вознесіння",
   xk: "Косово",
