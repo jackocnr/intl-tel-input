753bb6ce64b393c348f477fd11301beaf15e6fce
CorvS <w8.account@outlook.de>
2025-10-23 08:53:59 +0200

    Add alternative pluralisation logic for Romanian

--- src/js/intl-tel-input/i18n/ro/interface.ts
+++ src/js/intl-tel-input/i18n/ro/interface.ts
@@ -7,9 +7,17 @@ const interfaceTranslations: I18n = {
   countryListAriaLabel: "Lista țărilor",
   searchPlaceholder: "Căutare",
   zeroSearchResults: "Nici un rezultat gasit",
-  oneSearchResult: "1 rezultat găsit",
-  multipleSearchResults: "${count} rezultate găsite",
-  
+
+  searchResultsText(count) {
+    if (count === 1) {
+      return "1 rezultat găsit";
+    }
+
+    const isFew = count % 100 >= 1 && count % 100 <= 19;
+
+    return `${count}${isFew ? "" : " de"} rezultate găsite`;
+  },
+
   // additional countries (not supported by country-list library)
   ac: "Insula Ascensiunii",
   xk: "Kosovo",
