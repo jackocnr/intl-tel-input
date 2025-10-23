3332dc35cb5e9570945d11daabdc322d89d0f1cf
CorvS <w8.account@outlook.de>
2025-10-23 08:38:10 +0200

    Add alternative pluralisation logic for Czech

--- src/js/intl-tel-input/i18n/cs/interface.ts
+++ src/js/intl-tel-input/i18n/cs/interface.ts
@@ -7,9 +7,19 @@ const interfaceTranslations: I18n = {
   countryListAriaLabel: "Seznam zemí",
   searchPlaceholder: "Vyhledat",
   zeroSearchResults: "Nebyly nalezeny žádné výsledky",
-  oneSearchResult: "1 výsledek nalezen",
-  multipleSearchResults: "${count} výsledků nalezeno",
-  
+
+  searchResultsText(count) {
+    if (count === 1) {
+      return "Nalezen 1 výsledek";
+    }
+
+    if (count >= 2 && count <= 4) {
+      return `Nalezeny ${count} výsledky`;
+    }
+
+    return `Nalezeno ${count} výsledků`;
+  },
+
   // additional countries (not supported by country-list library)
   ac: "Ascension",
   xk: "Kosovo",
