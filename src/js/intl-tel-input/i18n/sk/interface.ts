7544c57374f53b6a79e1715192be15714c7475de
CorvS <w8.account@outlook.de>
2025-10-23 10:02:04 +0200

    Add alternative pluralisation logic for Slovak

--- src/js/intl-tel-input/i18n/sk/interface.ts
+++ src/js/intl-tel-input/i18n/sk/interface.ts
@@ -7,9 +7,22 @@ const interfaceTranslations: I18n = {
   countryListAriaLabel: "Zoznam krajín",
   searchPlaceholder: "Vyhľadať",
   zeroSearchResults: "Neboli nájdené žiadne výsledky",
-  oneSearchResult: "1 nájdený výsledok",
-  multipleSearchResults: "${count} nájdených výsledkov",
-  
+
+  searchResultsText(count) {
+    const mod10 = count % 10;
+    const mod100 = count % 100;
+
+    // Numbers ending in 1, but not 11
+    if (mod10 === 1 && mod100 !== 11) {
+      return `${count} výsledok nájdený`;
+    }
+
+    // Numbers ending in 2-4, but not 12-14
+    const isFew = mod10 >= 2 && mod10 <= 4 && !(mod100 >= 12 && mod100 <= 14);
+
+    return `${count} ${isFew ? "výsledky nájdené" : "výsledkov nájdených"}`;
+  },
+
   // additional countries (not supported by country-list library)
   ac: "Ascension",
   xk: "Kosovo",
