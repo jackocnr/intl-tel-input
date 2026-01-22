784c72fb99d0e5454f5866ffce5eac80cea349b3
Koala Yeung <koalay@gmail.com>
2026-01-22 10:22:12 +0800

    i18n: add Chinese (Hong Kong)
    * add translation for Traditional Chinese users in Hong Kong.

---
+++ src/js/intl-tel-input/i18n/zh-hk/interface.ts
@@ -0,0 +1,19 @@
+//* Chinese (Simplified). Translated by: Google Translate.
+import { I18n } from "../types";
+
+const interfaceTranslations: I18n = {
+  selectedCountryAriaLabel: "更改國家，選擇「${countryName}」（${dialCode}）",
+  noCountrySelected: "選擇國家",
+  countryListAriaLabel: "國家清單",
+  searchPlaceholder: "搜尋",
+  clearSearchAriaLabel: "清除搜尋",
+  zeroSearchResults: "未找到相關項目",
+  oneSearchResult: "找到 1 個相關項目",
+  multipleSearchResults: "找到 ${count} 個相關項目",
+
+  // additional countries (not supported by country-list library)
+  ac: "阿森松島",
+  xk: "科索沃",
+};
+
+export default interfaceTranslations;
