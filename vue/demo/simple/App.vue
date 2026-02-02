44adcd4bc6799ec1cbca9c83faa820c500c2dd41
Emil Carlsson <hello@emilcarlsson.se>
2026-02-02 10:06:59 +0100

    fix: Input value desync on paste when component  rerenders (#2115)
    - Ensure that the input value is always in sync with the component's internal state, especially after paste events and re-renders.
    - Remove lazy model binding to prevent inconsistencies between the displayed value and the actual value.
    - Add support for both `modelValue` and `value` props to enhance compatibility with different usage patterns.
    - Update relevant demos to reflect these changes and ensure proper functionality.

--- vue/demo/simple/App.vue
+++ vue/demo/simple/App.vue
@@ -1,11 +1,8 @@
 <script setup>
-  import IntlTelInput from "../../src/intl-tel-input/IntlTelInputWithUtils.vue";
+import IntlTelInput from "../../build/exports/IntlTelInputWithUtils";
+//import IntlTelInput from "../../src/intl-tel-input/IntlTelInputWithUtils.vue";
 </script>
 
 <template>
-  <IntlTelInput
-    :options="{
-      initialCountry: 'us',
-    }"
-  />
+  <IntlTelInput :options="{ initialCountry: 'us' }" />
 </template>
