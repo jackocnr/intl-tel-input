b0e2b90b6be40a376c0b69ccd0abc10732ee8223
Jack OConnor <jackocnr@gmail.com>
2026-02-21 18:40:54 +0000

    docs: fix: a11y issue with heading anchors

--- site/grunt/templateUtils.js
+++ site/grunt/templateUtils.js
@@ -139,15 +139,7 @@ const createMarkdownRenderer = ({ slugifyHeading = defaultSlugifyHeading } = {})
     typographer: true,
   }).use(markdownItAnchor, {
     slugify: slugifyHeading,
-    permalink: markdownItAnchor.permalink.ariaHidden({
-      placement: "before",
-      symbol: `
-        <svg class="iti-anchor-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="14" height="14" fill="currentColor" aria-hidden="true" focusable="false">
-          <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
-          <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
-        </svg>
-      `,
-    }),
+    permalink: markdownItAnchor.permalink.headerLink({ safariReaderFix: true }),
   });
 
   addDocOptionsLayoutPlugin(md);
