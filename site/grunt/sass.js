072f9215d95640b0d3588221c37aed9a1c5b1f65
Jack OConnor <jackocnr@gmail.com>
2026-03-11 08:28:20 +0000

    site: move css

--- site/grunt/sass.js
+++ site/grunt/sass.js
@@ -10,6 +10,7 @@ module.exports = function(grunt) {
       files: {
         'build/css/website.css': 'src/css/website.scss',
         'build/css/large_flags_overrides.css': 'src/css/large_flags_overrides.scss',
+        'build/css/highlightjs_overrides.css': 'src/css/highlightjs_overrides.scss',
         'build/css/playground.css': 'src/css/playground.scss',
         'build/css/docs.css': 'src/css/docs.scss',
         'build/css/homepage.css': 'src/css/homepage.scss',
