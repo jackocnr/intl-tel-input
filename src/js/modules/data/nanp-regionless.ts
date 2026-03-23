3c09e78a1f1df8655d4d49756169ec07f2e5844e
Jack OConnor <jackocnr@gmail.com>
2026-03-23 18:56:22 +0000

    efficiency improvement

--- src/js/modules/data/nanp-regionless.ts
+++ src/js/modules/data/nanp-regionless.ts
@@ -2,7 +2,7 @@ import { DIAL } from "../constants";
 import { getNumeric } from "../utils/string";
 
 //* https://en.wikipedia.org/wiki/List_of_North_American_Numbering_Plan_area_codes#Non-geographic_area_codes
-export const regionlessNanpNumbers = [
+const regionlessNanpNumbers = new Set([
   "800",
   "822",
   "833",
@@ -20,14 +20,14 @@ export const regionlessNanpNumbers = [
   "887",
   "888",
   "889",
-];
+]);
 
 //* Check if the given number is a regionless NANP number (expects the number to contain an international dial code)
 export const isRegionlessNanp = (number: string): boolean => {
   const numeric = getNumeric(number);
   if (numeric.startsWith(DIAL.NANP) && numeric.length >= 4) {
     const areaCode = numeric.substring(1, 4);
-    return regionlessNanpNumbers.includes(areaCode);
+    return regionlessNanpNumbers.has(areaCode);
   }
   return false;
 };
