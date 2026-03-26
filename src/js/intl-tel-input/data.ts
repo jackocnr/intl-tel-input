c8cea6cb4f199b9ef317fa5279880a5e49cb8d47
Jack OConnor <jackocnr@gmail.com>
2026-03-26 21:58:16 +0000

    Revert "enable tsconfig strict mode, and lots of fixes"
    This reverts commit 207e39198994d72a192e14621e5f6fe95cdea9fd.

--- src/js/intl-tel-input/data.ts
+++ src/js/intl-tel-input/data.ts
@@ -1447,7 +1447,7 @@ export type Country = {
   iso2: Iso2;
   dialCode: string;
   priority: number;
-  areaCodes: readonly string[] | null;
+  areaCodes: string[] | null;
   nationalPrefix: string | null;
 
   // the following fields are populated by the plugin
