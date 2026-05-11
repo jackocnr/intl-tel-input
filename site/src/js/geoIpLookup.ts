6805b790272ce37901a802fb4fee84f630ab1aa9
Jack OConnor <jackocnr@gmail.com>
2026-05-11 19:58:15 +0100

    Site: caching

--- site/src/js/geoIpLookup.ts
+++ site/src/js/geoIpLookup.ts
@@ -1,7 +1,18 @@
 import type { Iso2 } from "intl-tel-input";
 
+const STORAGE_KEY = "iti-geoip-country";
+
 export const geoIpLookup = async (): Promise<Iso2> => {
+  const cached = sessionStorage.getItem(STORAGE_KEY) as Iso2 | null;
+  if (cached) {
+    return cached;
+  }
+
   const res = await fetch(`https://ipapi.co/json?token=${process.env.IPAPI_TOKEN}`);
   const data = await res.json();
-  return data.country_code;
+  const country = data.country_code as Iso2;
+  if (country) {
+    sessionStorage.setItem(STORAGE_KEY, country);
+  }
+  return country;
 };
